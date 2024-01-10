f6217b255b6551141269d691fe5725dd8fc0593d
George Cesana <georgegeiwitzcesana@gmail.com>
2024-01-09 18:58:41 -0800

    ?

--- pygame_frontend/res.py
+++ pygame_frontend/res.py
@@ -1,6 +1,6 @@
 import platform
 SYSTEM = platform.system().lower()
-ENGINE_PATH = 'bin/ChesscadeLib.dll' if "windows" in SYSTEM else 'bin/ChesscadeLib.so' if "linux" in SYSTEM else 'bin/cygChesscadeLib.dll' if 'cygwin' in SYSTEM else ''
+ENGINE_PATH = 'bin/ChesscadeLib.dll' if "windows" in SYSTEM else 'bin/libChesscadeLib.so' if "linux" in SYSTEM else 'bin/cygChesscadeLib.dll' if 'cygwin' in SYSTEM else ''
 assert(ENGINE_PATH, f"Not compatible with {platform.system()} systems.")
 BOARD_PIECES_DIR = "assets/grey_pieces/"
 DECK_PIECES_DIR = "assets/black_white_pieces/"
