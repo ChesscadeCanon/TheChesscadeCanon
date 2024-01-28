d5d5942be8ed431e6e7c666168b45eaebf6ad505
George Cesana <georgegeiwitzcesana@gmail.com>
2024-01-27 23:17:18 -0800

    ?

--- src/standalone/main.c
+++ src/standalone/main.c
@@ -1,15 +1,50 @@
 #ifdef _WIN32
 #include "application.h"
 #endif
+#include "tests.h"
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void print_help() {
+
+#ifdef _WIN32
+	printf("Run with no arguments to play command line app.\n");
+#else
+	printf("Command line app only available on Windows. Only tests are available.\n");
+#endif
+	printf("Run with -t to run tests.\n");
+	printf("Run with -t [test number] to run a specific test.\n");
+	printf("Run with -h to see this message.\n");
+}
+
+Bool argmatch(int argc, const char** argv, int index, const char* arg) {
+
+	return argc > index && strncmp(arg, argv[index], 2);
+}
 
 int main(int argc, char** argv) {
 	if (argc == 1) {
 #ifdef _WIN32
 		run_pretty_text_game(DEFAULT_SETTINGS);
 #else
-		printf("Command line app only available on Windows. Run with -t to run tests.\n");
+		print_help();
 #endif
+	}
+	else if (argmatch(argc, argv, 1, "-h")) {
+
+		print_help();
+	}
+	else if (argmatch(argc, argv, 1, "-t")) {
+
+		int select = -1;
+
+		if (argc >= 3) {
+
+			select = atoi(argv[2]);
+		}
+
+		run_tests(select);
 	}
 	return 0;
 }
---
+++ src/standalone/tests.c
@@ -0,0 +1,14 @@
+#include "tests.h"
+#include <stdio.h>
+#define SELECT(__test_number__, __select__) {if(__test_number__ >= 0 && __test_number__ != __select__) return;}
+
+void _test_0(int select)
+{
+	SELECT(0, select);
+	printf("test 0\n");
+}
+
+void run_tests(int select) {
+
+	_test_0(select);
+}
