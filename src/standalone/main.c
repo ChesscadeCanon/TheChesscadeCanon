956afaf287d29c2c42c991cf6eb59dfa0a7e8b4d
George Cesana <georgegeiwitzcesana@gmail.com>
2024-01-27 22:30:16 -0800

    sorry

--- src/standalone/main.c
+++ src/standalone/main.c
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
 #ifdef _WIN32
 		run_pretty_text_game(DEFAULT_SETTINGS);
 #else
-		printf("Command line app only available on Windows. Run with -t to run tests.");
+		printf("Command line app only available on Windows. Run with -t to run tests.\n");
 #endif
 	}
 	return 0;
