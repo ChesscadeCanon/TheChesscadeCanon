9f5f11689f520e285e50b104b548383df3efe9a2
ChesscadeCanon <chesscadecanon@gmail.com>
2024-06-19 22:15:50 -0700

    basic board

--- godot_frontend/app/board.gd
+++ godot_frontend/app/board.gd
@@ -1,20 +1,34 @@
 extends Panel
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
+func _draw_piece_on_square(dir:StringName, white: bool, piece: StringName, square: Vector2i):
+	var texture := ChesscadeModel.piece(dir, white, piece)
+	draw_texture(texture, square * ChesscadeModel.SQUARE_LENGTH)
 
-	var sprite_map := ChesscadeModel.PIECE_MAP
+func _draw_landed_pieces()->void:
+	var board_size :Vector2i= ChesscadeModel.get_board_size()
+	var board_state_string :String= ChesscadeModel.get_board_state()
+	var board_state_array :PackedByteArray= board_state_string.to_ascii_buffer()
+	var index := 0
+	for p in board_state_string:
+		var square = Vector2i(index % (board_size.x + 1), index / (board_size.x + 1))
+		if p.to_lower() in ChesscadeModel.PIECE_NAMES: 
+			var white := p.to_upper() == p
+			var dir :=ChesscadeModel.BOARD_PIECES_DIR
+			_draw_piece_on_square(dir, white, p, square)
+		index += 1
+
+func _draw_player()->void:
+	var board_size :Vector2i= ChesscadeModel.get_board_size()
+	var player_square :Vector2i= ChesscadeModel.get_player_square()
+	var player_string :StringName= ChesscadeModel.get_player()
+	var white := player_string.to_upper() == player_string
+	var dir :=ChesscadeModel.BOARD_PIECES_DIR
+	_draw_piece_on_square(dir, white, player_string, player_square)
 
 func _process(delta)->void:
 	queue_redraw()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _draw()->void:
-	var board_size :Vector2i= ChesscadeModel.get_board_size()
-	var board_state_string :StringName= ChesscadeModel.get_board_state()
-	var board_state_array :PackedByteArray= board_state_string.to_ascii_buffer()
-	var player_square :Vector2i= ChesscadeModel.get_player_square()
-	var player_string :StringName= ChesscadeModel.get_player()
-	var player_texture :Texture2D= ChesscadeModel.PIECE_MAP[player_string.to_lower()]
-	draw_texture(player_texture, player_square * 16.0)
-	
+	_draw_player()
+	_draw_landed_pieces()
--- godot_frontend/app/chesscade_adapter.gd
+++ godot_frontend/app/chesscade_adapter.gd
@@ -1,23 +1,26 @@
 extends ChesscadeAdapter
 
-const KING := preload("res://bk.png")
-const QUEEN := preload("res://bq.png")
-const BISHOP := preload("res://bb.png")
-const KNIGHT := preload("res://bn.png")
-const ROOK := preload("res://br.png")
-const PAWN := preload("res://bp.png")
-var PIECE_MAP := {}
+const PIECE_NAMES := "bknpqr"
+const SQUARE_LENGTH := 16
+const BOARD_PIECES_DIR :StringName= "grey_pieces"
+const DECK_PIECES_DIR :StringName= "black_white_pieces"
+const NEXT_PIECES_DIR :StringName= "orange_pieces"
+const THREATENED_PIECES_DIR :StringName= "red_pieces"
+const SHADOW_PIECES_DIR :StringName= "shadow_pieces"
+const PLAYER_PIECES_DIR :StringName= "purple_pieces"
+const ARROW_PNG :StringName= "play.png"
+
+var _piece_textures := {}
+
+func piece(dir:StringName, white: bool, piece: StringName)->Texture2D:
+	var color := 'w' if white else 'b'
+	var path :StringName= 'res://assets/' + dir + '/' + color + piece.to_lower() + '.png'
+	if not _piece_textures.find_key(path):
+		_piece_textures[path] = load(String(path))
+	return _piece_textures[path]
 
 func debug_board()->void:
 	print(get_board_state())
-
-func _init():
-	PIECE_MAP['k'] = KING
-	PIECE_MAP['q'] = QUEEN
-	PIECE_MAP['b'] = BISHOP
-	PIECE_MAP['q'] = KNIGHT
-	PIECE_MAP['r'] = ROOK		
-	PIECE_MAP['p'] = PAWN
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
